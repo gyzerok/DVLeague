cbbb8ddf976c7ab934e2d7f4b181326f33cabea3
Fyodor Nezhivoy <gyzerok@gmail.com>
2012-05-29 20:01:38 +0600

    empty commit

--- index.php
+++ index.php
@@ -9,3 +9,4 @@ $loader = new Twig_Loader_Filesystem('view');
 $twig = new Twig_Environment($loader, array('cache' => 'twig_cache',));
 
 echo $twig->render('index.html', array('authed' => CUserController::Logged(), 'user_name' => $_SESSION['user_name']));
+?>
---
+++ registration.php
@@ -0,0 +1,12 @@
+<?php
+    session_start();
+    include ($_SERVER["DOCUMENT_ROOT"].'/controller/CUserController.php');
+    require_once '/import/twig/lib/Twig/Autoloader.php';
+
+    Twig_Autoloader::register();
+
+    $loader = new Twig_Loader_Filesystem('view');
+    $twig = new Twig_Environment($loader, array('cache' => 'twig_cache',));
+    echo $twig->render('registration_form.html', array());
+    //echo $twig->render('index.html', array('authed' => CUserController::Logged(), 'user_name' => $_SESSION['user_name']));
+?>
